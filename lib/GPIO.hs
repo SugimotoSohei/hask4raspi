175a8848e50709c83d0136702daacde5a71735c9
Sohei <1130071@stu.hus.ac.jp>
2015-02-19 14:11:17 +0900

    暫定動作テスト

--- lib/GPIO.hs
+++ lib/GPIO.hs
@@ -1,15 +1,18 @@
 module GPIO where
 
 data PinMode = IN | OUT deriving (Eq)
---data Pin = Pin Int deriving (Show,Eq)
-data Value = HIGH | LOW deriving (Show,Eq)
+data Value = HIGH | LOW deriving (Eq)
 
-type Pin = (Int,PinMode)
+type Pin = (PinNum,PinMode)
 type PinNum = Int
 
 instance Show PinMode where
   show IN  = "IN"
   show OUT  = "OUT"
+
+instance Show Value where
+  show HIGH = "1"
+  show LOW  = "0"
 
 pinAssign :: [PinNum]
 pinAssign = [2,3,4,7,8,9,10,11,14,15,17,18,22,23,24,25,27] ++ [5,6,12,13,16,20,21,26]
@@ -18,10 +21,28 @@ pinAssign = [2,3,4,7,8,9,10,11,14,15,17,18,22,23,24,25,27] ++ [5,6,12,13,16,20,2
 pin2Char :: PinNum -> String
 pin2Char n
   | n `elem` pinAssign = show n
-  | otherwise = error "no pin number"
+  | otherwise = error "no pin number, Stop ."
 
+open :: Pin -> IO ()
+open (x,y) = do
+  writeFile "./txt/test_open.txt" $ pin2Char x
+  writeFile "./txt/test_mode.txt" $ show y
 
-close :: PinNum -> IO ()
-close x = putStrLn $ "closepin = " ++ pin2Char x
+close :: Pin -> IO ()
+close (x,_) = writeFile "./txt/test_close.txt" $ pin2Char x
+
+closeNum :: PinNum -> IO ()
+closeNum x = writeFile "./txt/test_close.txt" $ pin2Char x
+
+pinOut :: Pin -> Value -> IO ()
+pinOut (x,y) z
+  | y == OUT = writeFile ("./txt/test_" ++ (pin2Char x) ++ "out.txt") $ show z
+  | otherwise = error "output error"
+
+--pinIn :: Pin -> IO String
+pinIn (x,y)
+  | y == IN = readFile "./txt/test_read.txt"
+  | otherwise = error "Read error"
 
 -- gpiofile "/sys/class/gpio"
+
--- lib/Test_gpio.hs
+++ lib/Test_gpio.hs
@@ -1,5 +1,12 @@
 import GPIO
 
 main = do
-  close 2
-  close 1
+  open (2,OUT)
+  open (3,IN)
+  pinOut (2,OUT) HIGH
+  a <- pinIn  (2,IN)
+  print a
+  close (2,OUT)
+  close (3,IN)
+  --close 1 -- no pin
+  print "OK"
